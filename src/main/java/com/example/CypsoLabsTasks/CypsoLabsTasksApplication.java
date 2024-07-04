b8f2f3258763bff5116d44121766f58e47ec3fa0
RavinduBA <e19004@eng.pdn.ac.lk>
2024-07-04 18:19:35 +0530

    Spring project added

---
+++ src/main/java/com/example/CypsoLabsTasks/CypsoLabsTasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.CypsoLabsTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CypsoLabsTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CypsoLabsTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CypsoLabsTasks/CypsoLabsTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CypsoLabsTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypsoLabsTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
